14bdb16f7257768652fe8956077198d03692a13e
michaldobiezynski <dobiezynski@gmail.com>
2020-01-03 09:12:11 +0000

    get all profiles and get a profile by user id routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -109,4 +109,38 @@ router.post(
   },
 );
 
+// @route   Get api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   Get api/profile/user/:user_id
+// @desc    Get profile by userID
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
