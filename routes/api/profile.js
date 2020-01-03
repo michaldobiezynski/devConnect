4daf972c7e9bc63790c6efd26c849b197532d299
michaldobiezynski <dobiezynski@gmail.com>
2020-01-03 10:57:30 +0000

    created route for deleting profile & user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -143,4 +143,26 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user & posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    //@todo - remove users posts
+
+    //Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
