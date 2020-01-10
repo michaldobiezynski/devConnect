3113dcefa5e0171d66e9d39e69706b705d91d185
michaldobiezynski <dobiezynski@gmail.com>
2020-01-10 19:24:33 +0000

    starting on the dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,6 +7,7 @@ import {
   LOGIN_FAIL,
   LOGIN_SUCCESS,
   LOGOUT,
+  CLEAR_PROFILE,
 } from './types';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
@@ -95,5 +96,6 @@ export const login = (email, password) => async dispatch => {
 
 //Logout /Clear Profile
 export const logout = () => dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
